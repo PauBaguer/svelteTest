d0a5c984e712facfff8e5b436c8d23a0746af36f
Pau Baguer <pauinki@gmail.com>
2022-04-07 23:23:35 +0200

    layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Card from '@smui/card';
+	import Textfield from '@smui/textfield';
+	import Icon from '@smui/textfield/icon';
+	import HelperText from '@smui/textfield/helper-text';
+	import Button, { Label } from '@smui/button';
+	import Banner, { CloseReason } from '@smui/banner';
+</script>
+
+<div class="header">
+	<a href="/">
+		<img src="/logo.png" alt="BookHub" width="120px" />
+	</a>
+	<div>
+		<Button color="secondary" on:click={() => (location.href = '/login')} variant="outlined">
+			<Label>Login</Label>
+		</Button>
+		<Button color="secondary" on:click={() => (location.href = '/login')} variant="outlined">
+			<Label>Register</Label>
+		</Button>
+	</div>
+</div>
+
+<div class="card-display">
+	<div>
+		<h1 class="mdc-typography--headline1">Welcome to BookHub!</h1>
+	</div>
+	<h3>the reader's social network</h3>
+</div>
+
+<style>
+	.header {
+		top: 0px;
+		background-color: #2996fc;
+		right: 0px;
+		left: 0px;
+		position: absolute;
+		z-index: 100;
+		display: flex;
+
+		justify-content: space-between;
+		align-items: center;
+
+		padding: 10px 10px 10px 10px;
+		gap: 10px;
+	}
+
+	img {
+		flex: none;
+	}
+	.card-display {
+		text-align: center;
+
+		font-size: xx-large;
+		position: absolute;
+		top: 0px;
+		bottom: 0px;
+		right: 0px;
+		left: 0px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		flex-direction: column;
+	}
+</style>
---
+++ src/routes/login.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Card from '@smui/card';
+	import Textfield from '@smui/textfield';
+	import Icon from '@smui/textfield/icon';
+	import HelperText from '@smui/textfield/helper-text';
+	import Button, { Label } from '@smui/button';
+
+	let username = '';
+	let password = '';
+
+	function login() {}
+</script>
+
+<div class="card-display">
+	<div><h1>Welcome to BookHub</h1></div>
+	<div class="card-container">
+		<Card padded>
+			<h4>Login</h4>
+			<Textfield class="shaped-outlined" variant="outlined" bind:value={username} label="Username">
+				<Icon class="material-icons" slot="trailingIcon">person</Icon>
+				<HelperText slot="helper">Enter your account username!</HelperText>
+			</Textfield>
+			<Textfield class="shaped-outlined" variant="outlined" bind:value={password} label="Password">
+				<Icon class="material-icons" slot="trailingIcon">key</Icon>
+				<HelperText slot="helper">Enter your accound password!</HelperText>
+			</Textfield>
+			<Button on:click={login} variant="raised">
+				<Label>Login</Label>
+			</Button>
+		</Card>
+	</div>
+</div>
+
+<style>
+	.card-display {
+		text-align: center;
+		font: 1em roboto;
+		position: absolute;
+		top: 0px;
+		bottom: 0px;
+		right: 0px;
+		left: 0px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		flex-direction: column;
+	}
+</style>
